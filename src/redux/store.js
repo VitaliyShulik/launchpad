152c451e92721d4671eb5c3f3c6d4a893ba65ef4
Vitaliy Shulik <shulikvitalya@gmail.com>
2022-11-11 18:53:10 +0600

    add useAppData hook and add mock appData store

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,11 +3,13 @@ import thunk from "redux-thunk";
 import blockchainReducer from "./blockchain/blockchainReducer";
 import contractReducer from "./contract/contractReducer";
 import userDataReducer from "./userData/dataReducer";
+import appSettingsReducer from './appSettings/reducer';
 
 const rootReducer = combineReducers({
   blockchain: blockchainReducer,
   userData: userDataReducer,
   contract: contractReducer,
+  appSettings: appSettingsReducer,
 });
 
 const middleware = [thunk];
